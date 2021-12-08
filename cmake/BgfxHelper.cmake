9d5603403eb974c2d9901d4595a39772abf573ff
Martin Chang <marty1885@users.noreply.github.com>
2021-12-08 19:26:14 +0800

    improve shader compilation handling

--- cmake/BgfxHelper.cmake
+++ cmake/BgfxHelper.cmake
@@ -1,29 +1,26 @@
 include( CMakeParseArguments )
 
 # Adds a folder full of shaders for compilation
-function(bgfx_add_shaders compile_target source_dir target_dir)
+function(bgfx_add_shaders compile_target source_dir)
     file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${source_dir})
     file(GLOB_RECURSE SHADER_LIST ${source_dir}/*.sc)
     set(SHADER_OUTPUT_LIST "")
     foreach(shader_source ${SHADER_LIST})
-         file(RELATIVE_PATH
+        file(RELATIVE_PATH
              input_file
              ${CMAKE_CURRENT_SOURCE_DIR}
              ${shader_source})
-        get_filename_component( filename "${shader_source}" NAME_WE )
-        if(filename STREQUAL "varying")
+        get_filename_component( filename "${shader_source}" NAME_WLE )
+        if(filename STREQUAL "varying.def")
             continue()
         endif()
-        string( SUBSTRING "${filename}" 0 2 TYPE )
-        if( "${TYPE}" STREQUAL "fs" )
+        string( SUBSTRING "${filename}" 0 3 TYPE )
+        if( "${TYPE}" STREQUAL "fs_" )
             set( TYPE "FRAGMENT" )
-            set( D3D_PREFIX "ps" )
-        elseif( "${TYPE}" STREQUAL "vs" )
+        elseif( "${TYPE}" STREQUAL "vs_" )
             set( TYPE "VERTEX" )
-            set( D3D_PREFIX "vs" )
-        elseif( "${TYPE}" STREQUAL "cs" )
+        elseif( "${TYPE}" STREQUAL "cs_" )
             set( TYPE "COMPUTE" )
-            set( D3D_PREFIX "cs" )
         else()
             message(FATAL_ERROR "Shaders filename must have fs_, ps_ or vs_ prefix. Got ${filename}")
         endif()
